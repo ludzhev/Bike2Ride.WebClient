1c28782f9cf67127015bc7adb39ba846e72bbce0
ludzhev <ludzhev@gmail.com>
2017-10-19 15:34:03 +0300

    Move WebClient project in separate folder.

--- Bike2Ride/Web/Bike2Ride.WebClient/App_Start/NinjectWebCommon.cs
+++ Bike2Ride/Web/Bike2Ride.WebClient/App_Start/NinjectWebCommon.cs
@@ -61,6 +61,7 @@ namespace Bike2Ride.WebClient.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+
         }        
     }
 }
