a0134e1f4e89dafcfecd547c98179f1f0044d8df
ludzhev <ludzhev@gmail.com>
2017-10-21 22:29:09 +0300

    Add Home page paralax.

--- Bike2Ride/Web/Bike2Ride.WebClient/Scripts/init.js
+++ Bike2Ride/Web/Bike2Ride.WebClient/Scripts/init.js
@@ -1,8 +1,17 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+      $(".button-collapse").sideNav();
+      $(".parallax").parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
+
+$(document).ready(function () {
+    $(".js-scrollTo").on("click", function () {
+        var page = $(this).attr("href");
+        var speed = 750;
+        $("html, body").animate({ scrollTop: $(page).offset().top }, speed);
+        return false;
+    });
+});
