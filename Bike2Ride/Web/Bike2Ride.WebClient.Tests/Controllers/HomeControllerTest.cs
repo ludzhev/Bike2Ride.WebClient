2ccf18c969202f8815df3f55bd8bd3267d98f086
ludzhev <ludzhev@gmail.com>
2017-10-23 11:31:43 +0300

    Include testing packages.

--- Bike2Ride/Web/Bike2Ride.WebClient.Tests/Controllers/HomeControllerTest.cs
+++ Bike2Ride/Web/Bike2Ride.WebClient.Tests/Controllers/HomeControllerTest.cs
@@ -5,8 +5,23 @@ using System.Text;
 using System.Web.Mvc;
 using Bike2Ride.WebClient;
 using Bike2Ride.WebClient.Controllers;
+using Moq;
+using NUnit.Framework;
 
 namespace Bike2Ride.WebClient.Tests.Controllers
 {
-    
+    [TestFixture]
+    public class Index_Should
+    {
+        [Test]
+        public void RenderHomePage()
+        {
+            const string result = "work";
+
+            var testMock = new Mock<HomeController>();
+            testMock.Verify();
+
+            Assert.AreEqual(result, result);
+        }
+    }
 }
