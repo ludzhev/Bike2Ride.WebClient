72a0d26581a4dd18c1500c45138285a337f8c310
ludzhev <ludzhev@gmail.com>
2017-10-24 05:12:45 +0300

    Inject ICityService in MapController.

--- Bike2Ride/Web/Bike2Ride.WebClient/Controllers/MapController.cs
+++ Bike2Ride/Web/Bike2Ride.WebClient/Controllers/MapController.cs
@@ -1,47 +1,60 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
+using Bike2Ride.Services.Contracts;
 using Bike2Ride.WebClient.ViewModels;
+
+using Bytes2you.Validation;
 
 namespace Bike2Ride.WebClient.Controllers
 {
     public class MapController : Controller
     {
+        private const string IndexTitle = "Show Stations";
+        private const string DefaultCity = "Sofia";
+        private const int DefaultZoomLevel = 12;
+        private const int CacheDuration = 60 * 60 * 24;
+
+        private readonly ICityService cityService;
+
+        public MapController(ICityService cityService)
+        {
+            Guard.WhenArgument(cityService, "MapController cityService")
+                .IsNull()
+                .Throw();
+
+            this.cityService = cityService;
+        }
+
+        [OutputCache(
+            Location = System.Web.UI.OutputCacheLocation.Server,
+            Duration = CacheDuration)]
         public ActionResult Index()
         {
+            var city = this.cityService.GetCityByName(DefaultCity);
+
             var model = new MapViewModel()
             {
-                Title = "Show Stations",
+                Title = IndexTitle,
                 Center = new LocationViewModel()
                 {
-                    lat = 42.70,
-                    lng = 23.317
+                    lat = city.Center.Lat,
+                    lng = city.Center.Lng
                 },
-                ZoomLevel = 12,
-                Stations = new List<LocationViewModel>()
-                {
-                    new LocationViewModel()
-                    {
-                        lat = 42.705,
-                        lng = 23.297
-                    },
-                    new LocationViewModel()
-                    {
-                        lat = 42.690,
-                        lng = 23.337
-                    },
-                    new LocationViewModel()
-                    {
-                        lat = 42.651,
-                        lng = 23.379
-                    },
-                    new LocationViewModel()
-                    {
-                        lat = 42.674,
-                        lng = 23.310
-                    }
-                }
+                ZoomLevel = city.ZoomLevel ?? DefaultZoomLevel,
             };
+
+            var locations = city
+                .Locations.Select(location => new LocationViewModel()
+                {
+                    lat = location.Lat,
+                    lng = location.Lng
+                })
+                .ToList();
+
+            model.Stations = locations;
+
             return this.View(model);
         }
     }
