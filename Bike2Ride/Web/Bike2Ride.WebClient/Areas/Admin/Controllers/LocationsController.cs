1fc33d4646a68fe62feb42e2d7899084accfac03
ludzhev <ludzhev@gmail.com>
2017-10-24 09:48:03 +0300

    Add Admin area.

---
+++ Bike2Ride/Web/Bike2Ride.WebClient/Areas/Admin/Controllers/LocationsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Bike2Ride.WebClient.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class LocationsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
